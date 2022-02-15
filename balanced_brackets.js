d8907f34263a4885fe13ac1654476f4a0debcb9b
twknab <20636750+twknab@users.noreply.github.com>
2022-02-14 18:24:10 -0800

    added balanced brackets

---
+++ balanced_brackets.js
@@ -0,0 +1,64 @@
+/*
+Given a string of brackets, e.g, "{([])}", write a function 
+`balancedBrackets(string)` that returns a `boolean` if the brackets are balanced.
+
+Example)
+balancedBrackets("{([])}");
+
+Output:
+true
+
+balancedBrackets("{([)}");
+
+Output:
+false
+*/
+
+function balancedBrackets(string) {
+  let queue = [];
+  const pairings = {
+    "{": "}",
+    "[": "]",
+    "(": ")",
+  };
+  
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+    // Add opening bracket to queue if detected
+    if (char in pairings) {
+      queue.push(char);
+    }
+    // Remove from queue if matching closing bracket is detected
+    else {
+      if (queue.length > 0 && Object.values(pairings).includes(char)) {
+        let last = queue.pop();
+        if (pairings[last] !== char) {
+          // Brackets are not balanced
+          return false;
+        }
+      } 
+    }
+  }
+  // Brackets are balanced
+  return true;
+}
+
+let bracketsString = "{([])}";
+let areBracketsBalanced = balancedBrackets(bracketsString);
+console.log(areBracketsBalanced);
+// true
+
+bracketsString = "{([)}";
+areBracketsBalanced = balancedBrackets(bracketsString);
+console.log(areBracketsBalanced);
+// false
+
+bracketsString = "({[{[]}]})";
+areBracketsBalanced = balancedBrackets(bracketsString);
+console.log(areBracketsBalanced);
+// true
+
+bracketsString = "({[{[]]})";
+areBracketsBalanced = balancedBrackets(bracketsString);
+console.log(areBracketsBalanced);
+// false
